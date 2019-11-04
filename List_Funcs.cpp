8d0159dffff23ab473b4e85c487e90c8654fae39
kostya2709 <k.dragun.k@mail.ru>
2019-11-05 01:34:59 +0300

    Insert_After is added. Dump is improved.

--- List_Funcs.cpp
+++ List_Funcs.cpp
@@ -8,26 +8,75 @@ int List_Construct (List* list1)
     list1->next = (int*)calloc (1, list_start_size * sizeof (int));
     list1->prev = (int*)calloc (1, list_start_size * sizeof (int));
 
-    list1->size = list_start_size;
+    list1->size = 0;
     list1->max_size = list_start_size;
-    list1->head = 1;
-    list1->tail = 1;
+    list1->head = 0;
+    list1->tail = 0;
     list1->free = 1;
 
     int i = 0;
     *list1->prev = 0;
     *(list1->prev + 1) = 0;
-    for (i = 2; i < list1->size; i++)
+    for (i = 2; i < list1->max_size; i++)
         *(list1->prev + i) = POISON;
 
     *list1->next = 0;
+    *(list1->next + list1->max_size) = 0;
 
-    for (i = 1; i < list1->size; i++)
+    for (i = 1; i < list1->max_size - 1; i++)
         *(list1->next + i) = i + 1;
 
 
 
     return 0;
+}
+
+int Insert_After (List* list1, int location, double elem)
+{
+
+    assert (list1);
+
+    list1->size += 1;
+    int pos = list1->free;
+    list1->free = *(list1->next + pos);
+
+    *(list1->data + pos) = elem;
+
+
+    if (location == 0)
+    {
+        int ex_first = list1->head;
+        list1->head = pos;
+
+        *(list1->next + pos) = ex_first;
+        *(list1->prev + pos) = 0;
+
+        if (ex_first)
+            *(list1->prev + ex_first) = pos;
+
+        if (list1->tail == 0)
+            list1->tail = pos;
+
+    }
+
+    else
+    {
+        if (location == list1->tail)
+            list1->tail = pos;
+
+        int next_el = *(list1->next + location);
+        *(list1->next + location) = pos;
+
+        *(list1->prev + pos) = location;
+        *(list1->next + pos) = next_el;
+        if (next_el)
+            *(list1->prev + next_el) = pos;
+    }
+
+
+
+    return 0;
+
 }
 
 
@@ -39,19 +88,20 @@ int List_Dump (List* list1, char* list_name)
     printf ("\tList elements:\n");
 
     int i = 0;
-    for (i = 0; i < list1->size; i++)
+    for (i = 0; i < list1->max_size; i++)
         printf ("\t\tdata [%d] = %.lf\n", i, *(list1->data + i));
 
     printf ("\n\tArray of next elements:\n");
-    for (i = 0; i < list1->size; i++)
-        printf ("\t\tnext [%d] = %d\n", i, *(list1->prev + i));
+    for (i = 0; i < list1->max_size; i++)
+        printf ("\t\tnext [%d] = %d\n", i, *(list1->next + i));
 
     printf ("\n\tArray of previous elements:\n");
-    for (i = 0; i < list1->size; i++)
-        printf ("\t\tprev [%d] = %d\n", i, *(list1->next + i));
+    for (i = 0; i < list1->max_size; i++)
+        printf ("\t\tprev [%d] = %d\n", i, *(list1->prev + i));
 
     printf ("\n\nList head: %d\n", list1->head);
     printf ("List tail: %d\n", list1->tail);
     printf ("List free: %d\n\n\n", list1->free);
 
+    return 0;
 }
